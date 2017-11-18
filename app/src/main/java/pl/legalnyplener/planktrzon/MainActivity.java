742514c105aa27a89c122c75e5d204265f2213cb
Kuba Steindel <steindel@live.com>
2017-11-18 16:22:52 +0100

    + Activity Logowania + połączenie z serwerem (pobranie danych z bazy)

--- app/src/main/java/pl/legalnyplener/planktrzon/MainActivity.java
+++ app/src/main/java/pl/legalnyplener/planktrzon/MainActivity.java
@@ -1,13 +1,74 @@
 package pl.legalnyplener.planktrzon;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+    private String downloaded_data;
+    private boolean loaded = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new PobierzPlakaty().execute();
+    }
+
+    public void GoInButtonClick(View view){
+        Intent intent = new Intent();
+
+    }
+
+    public void LogInButtonClick(View view){
+
+    }
+
+    public class PobierzPlakaty extends AsyncTask<Void, Void, String>{
+        private String LINK_URL = "http://plakat.legalnyplener.pl/SiiHackaton/pobierz_plakaty.php";;
+        @Override
+        protected void onPostExecute(String s) {
+            downloaded_data = s;
+            loaded = true;
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            return DownloadData(LINK_URL);
+        }
+
+        private String DownloadData(String adress) {
+            String data = new String();
+            try {
+                URL url = new URL(adress);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                httpURLConnection.setDoInput(true);
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
+                String temp;
+                while ((temp = bufferedReader.readLine()) != null){
+                    data += temp;
+                }
+                bufferedReader.close();
+                inputStream.close();
+                httpURLConnection.disconnect();
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return data;
+        }
     }
 }
